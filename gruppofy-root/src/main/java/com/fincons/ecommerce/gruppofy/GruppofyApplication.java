0d6c1282b0504f2bdacb3509324ea6edf307f07f
Yaroslav Makhota <yaroslav.makhota@finconsgroup.com>
2026-03-20 12:07:27 +0100

    Creata e definita la struttura del progetto

---
+++ gruppofy-root/src/main/java/com/fincons/ecommerce/gruppofy/GruppofyApplication.java
@@ -0,0 +1,13 @@
+package com.fincons.ecommerce.gruppofy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GruppofyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GruppofyApplication.class, args);
+	}
+
+}
---
+++ gruppofy-root/src/test/java/com/fincons/ecommerce/gruppofy/GruppofyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fincons.ecommerce.gruppofy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GruppofyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

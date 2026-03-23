9aa416ff69f46e3bb67df4895d7539d9a4081788
Yaroslav Makhota <yaroslav.makhota@finconsgroup.com>
2026-03-23 10:20:20 +0100

    Merged featureFede to develop.

--- gruppofy-root/prodotti/src/main/java/com/fincons/ecommerce/prodotti/exception/NotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.fincons.ecommerce.prodotti.exception;
-
-public class NotFoundException extends Exception {
-
-    public NotFoundException(String message) {
-        super(message);
-    }
-}
